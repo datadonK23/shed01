ff0182e1c642cc350d9640a9c19a92bc01254b4b
donk23 <datadonk23@gmail.com>
2015-05-11 21:15:16 +0200

    mining activities

--- PyBeispiele/spyreTest/fetch_data.py
+++ PyBeispiele/spyreTest/fetch_data.py
@@ -1,8 +1,8 @@
 """
 Name: 
-Purpose: 
+Purpose: Fetching my data from Strava
 Author: Thomas Treml (datadonk23@gmail.com)
-Date: 
+Date: 2015-05-11
 """
 
 from stravalib import Client
@@ -24,9 +24,26 @@ for b in my_bikes:
     bike[b.id] = b.name
     bikes.update(bike)
 me["bikes"] = bikes
+#print me
 
-my_activities = client.get_activities(limit=1)
-result = my_activities.result_fetcher()
+# Fetch activities
+my_activities = client.get_activities(limit=5)
+act = []
+for a in my_activities:
+    act.append(a)
 
-print me
-print result
+last_id = act[0].id
+last_activity = client.get_activity(last_id)
+
+# Activity collection prototyp
+my_activity = {}
+if last_activity.type == "Ride":
+    my_activity["time"] = last_activity.moving_time
+    my_activity["distance"] = last_activity.distance
+    my_activity["avg_speed"] = last_activity.average_speed
+    my_activity["energy"] = last_activity.kilojoules
+else:
+    pass
+
+print my_activity
+
