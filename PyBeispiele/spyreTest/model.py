eb97775314182aaaa018be8feee7667229af8c17
donk23 <datadonk23@gmail.com>
2015-05-19 15:36:16 +0200

    data cleaning

--- PyBeispiele/spyreTest/model.py
+++ PyBeispiele/spyreTest/model.py
@@ -6,6 +6,7 @@ Date: 2015-05-18
 
 import os
 import pandas as pd
+from datetime import datetime, timedelta
 
 DATA_PATH = "./data/cycling_coll.json" #os.path.join(os.path.dirname(os.path.realpath(__file__)), "data", "cycling_coll.json")
 
@@ -13,3 +14,9 @@ raw = pd.read_json(DATA_PATH)
 
 # Cleaning
 raw["distance"] = raw["distance"].map(lambda x: float(x.split()[0]))
+raw["time_proc"] = raw["time"].map(lambda x: datetime.strptime(x, "%H:%M:%S"))
+raw["time"] = raw["time_proc"].map(lambda x: timedelta(hours=x.hour, minutes=x.minute, seconds=x.second))
+raw.drop("time_proc", 1)
+
+
+
