727f3ee4f41f7068daa43f9c7df4e7466ec358a2
donk23 <datadonk23@gmail.com>
2015-08-17 16:12:08 +0200

    further exploration on book data

--- PyBeispiele/BookRec/exploration.py
+++ PyBeispiele/BookRec/exploration.py
@@ -9,16 +9,14 @@ import numpy as np
 import pandas as pd
 
 raw_users = pd.read_csv("./data/BX-Users.csv", sep=";")
-print raw_users.head()
-print raw_users.tail(2)
 print raw_users.count()
 
 raw_ratings = pd.read_csv("./data/BX-Book-Ratings.csv", sep=";")
-print raw_ratings.head()
-print raw_ratings.tail(2)
 print raw_ratings.count()
 
 raw_books = pd.read_csv("./data/BX-Books.csv", sep=";", error_bad_lines=False) # 22 books skipped
-print raw_ratings.head()
-print raw_ratings.tail(2)
 print raw_books.count()
+
+print raw_books.head(2)
+print raw_users.head(2)
+print raw_ratings.head(2)
