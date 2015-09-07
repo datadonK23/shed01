f0ca03de0549bb14fa03e46677703e92dcfef105
donk23 <datadonk23@gmail.com>
2015-09-07 16:33:20 +0200

    db connect, input section

--- PyBeispiele/SherlockRec/app/controller.py
+++ PyBeispiele/SherlockRec/app/controller.py
@@ -2,23 +2,26 @@
 Controller
 """
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, sql
 
 class DB_Connection:
     def __init__(self):
         from instance.config import PG_USER, PG_PASSWORD
         db_uri = "postgresql://" + PG_USER + ":" + PG_PASSWORD + "@localhost/shrecdb"
-        engine = create_engine(db_uri)
-        self.connection = self.make_connection(engine)
-#FIXME
-    def make_connection(self, engine):
-        connection = engine.connect()
-        return connection
+        self.engine = create_engine(db_uri)
 
-    def close_connection(self):
-        connection.close()
+    def make_connection(self):
+        self.connection = self.engine.connect()
+        return self.connection
 
-#query_books = "SELECT * FROM books"
-#books_con = DB_Connection.make_connection()
-#books = books_con.execute(sql.text(query_books))
-#books_con.close_connection()
+    def close_connection(self, connection):
+        return connection.close()
+
+""" test
+query_books = "SELECT * FROM books"
+DB = DB_Connection()
+books_con = DB_Connection.make_connection(DB)
+books = books_con.execute(sql.text(query_books))
+DB_Connection.close_connection(DB, books_con)
+"""
+
--- PyBeispiele/SherlockRec/app/views.py
+++ PyBeispiele/SherlockRec/app/views.py
@@ -2,7 +2,7 @@
 Views
 """
 
-from flask import render_template
+from flask import render_template, request
 from app import app#, connection
 #from models import Features
 
@@ -11,3 +11,20 @@ from app import app#, connection
 def index(): 
     return render_template("index.html")
 
+@app.route("/survey")
+def survey():
+    return render_template("survey.html")
+
+@app.route("/ref")
+def ref():
+    return render_template("ref.html")
+
+#FIXME
+@app.route("/recommend")
+def recommend():
+    return render_template("index.html")
+
+#FIXME
+@app.route("/result")
+def result():
+    return render_template("index.html")
