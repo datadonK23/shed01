88fb249bd9583a38189efdbfa788af7b8fcea33e
donk23 <datadonk23@gmail.com>
2015-09-14 19:50:26 +0200

    simple input to engine logic

--- PyBeispiele/SherlockRec/app/views.py
+++ PyBeispiele/SherlockRec/app/views.py
@@ -2,7 +2,7 @@
 Views
 """
 
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from app import app#, connection
 #from models import Features
 
@@ -13,18 +13,28 @@ def index():
 
 @app.route("/survey")
 def survey():
-    return render_template("survey.html")
+    ratings = {}
+    if request.args:
+        ratings["input_book01"] = request.args.get("INBook01")
+        ratings["input_book02"] = request.args.get("INBook02")
+        ratings["input_book03"] = request.args.get("INBook03")
+        #test print ratings
+        return redirect(url_for("recommendation", ratings=ratings))
+    else:
+        return render_template("survey.html")
 
+
+#FIXME
+@app.route("/recommendation/<ratings>/")
+@app.route("/recommendation")
+def recommendation(ratings=None):
+    if ratings:
+        print ratings
+        return redirect(url_for("recommendation"))
+    return render_template("recommendation.html")
+
+
+#FIXME
 @app.route("/ref")
 def ref():
     return render_template("ref.html")
-
-#FIXME
-@app.route("/recommend")
-def recommend():
-    return render_template("index.html")
-
-#FIXME
-@app.route("/result")
-def result():
-    return render_template("index.html")
